c97d3d4f4bcbe6626dead8d566e1beb2707241c3
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-04-17 10:15:56 +0400

    !

--- Files.py
+++ Files.py
@@ -1,1 +1,15 @@
 print("Working with files")
+
+#with open("ReadFile.txt", "r") as readFile:
+    #print("Working with a file ReadFile.txt")
+
+try:
+    readFile = open("ReadFile.txt", "r")
+    try:
+        print("Working with a file ReadFile.txt")
+        for line in readFile:
+            print(line, end="")
+    finally:
+        readFile.close()
+except Exception as ex:
+    print("Error", ex)
