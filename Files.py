6cd2e25f82a10112e8a598c614bc8cd4fd6dd1e4
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-04-17 10:58:30 +0400

    !

--- Files.py
+++ Files.py
@@ -1,8 +1,6 @@
 print("Working with files")
 
-#with open("ReadFile.txt", "r") as readFile:
-    #print("Working with a file ReadFile.txt")
-
+"""
 try:
     readFile = open("ReadFile.txt", "r")
     try:
@@ -13,3 +11,55 @@ try:
         readFile.close()
 except Exception as ex:
     print("Error", ex)
+"""
+
+"""
+try:
+    with open("ReadFile.txt", "r") as readFile:
+        print("Reading a file ReadFile.txt")
+        for line in readFile:
+            print(line, end="")
+except FileNotFoundError as ex:
+    print("Error", ex)
+
+try:
+    with open("WriteFile.txt", "w") as writeFile:
+        print("Writing a file WriteFile.txt")
+        writeFile.write("hello world")
+        print("File written successfully!")
+except FileNotFoundError as ex:
+    print("Error", ex)
+
+try:
+    with open("WriteFile.txt", "a") as writeFile:
+        print("Writing a file WriteFile.txt")
+        writeFile.write("\nhello work")
+        print("File modified successfully!")
+except FileNotFoundError as ex:
+    print("Error", ex)
+"""
+
+fileName = "testFile.txt"
+
+def write():
+    message = input("Enter the string: ")
+    with open(fileName,"a") as file:
+        file.write(message + "\n")
+
+def read():
+    with open(fileName, "r") as file:
+        for text in file:
+            print(text, end = "")
+    print()
+
+while(True):
+    selection = int(input("1. Write to file\t\t2. Reading a file\t\t3. Exit\nSelect action: "))
+    match selection:
+        case 1: write()
+        case 2: read()
+        case 3: break
+        case _: print("Invalid input!")
+
+print("The program is completed!")
+
+
